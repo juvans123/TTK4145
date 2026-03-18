02f73a85a4015357f1cf3b4adc66dec31784f241
hannhaab <hanne@habrekke.no>
2026-03-18 22:37:30 +0100

    (lPhase == Unconfirmed || Phase == Served)

--- ordermanagement/ordermanager.go
+++ ordermanagement/ordermanager.go
@@ -151,7 +151,7 @@ mainLoop:
 			localOrder.SeenBy[myID] = true
 			localOrderView[key] = localOrder
 
-			if localOrder.Phase == Unconfirmed && !allAliveHaveSeen(localOrder.SeenBy, worldState.Alive) {
+			if (localOrder.Phase == Unconfirmed || localOrder.Phase == Served) && !allAliveHaveSeen(localOrder.SeenBy, worldState.Alive) {
 				continue mainLoop
 			}
 
